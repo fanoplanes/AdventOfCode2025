af7bf7454d940f9221270ed4e3525ddf4cc31732
FanoPlanes <pontryagin@protonmail.com>
2025-12-04 18:20:51 +0100

    Actually finished day 4.

--- 04/04-2.rkt
+++ 04/04-2.rkt
@@ -1,8 +1,6 @@
-TODO: unfinished
-
 #lang racket
 
-(define input (map string->list (file->lines "toy-input")))
+(define input (map string->list (file->lines "input")))
 
 (define height (length input))
 (define width (length (car input)))
@@ -10,38 +8,43 @@ TODO: unfinished
 (define (access x y)
   (list-ref (list-ref input y) x))
 
+(define hash-input (make-hash))
+(for* ([x width]
+       [y height])
+  (hash-set! hash-input (list x y) (access x y)))
+
 (define (rem x y)
-  (list-set input y (list-set (list-ref input y) x #\.)))
+  (hash-set! hash-input (list x y) #\.))
 
 (define (in-bounds? x y)
-  (cond [(< x 0) #f]
-        [(>= x width) #f]
-        [(< y 0) #f]
-        [(>= y height) #f]
-        [else #t]))
+  (cond
+    [(< x 0) #f]
+    [(>= x width) #f]
+    [(< y 0) #f]
+    [(>= y height) #f]
+    [else #t]))
 
 (define (roll? x y)
-  (if (char=? (access x y) #\@) 1 0))
+  (if (char=? (hash-ref hash-input (list x y)) #\@) 1 0))
 
 (define (neighbors x y)
-  (for*/sum ([a '(-1 0 1)]
-              [b '(-1 0 1)]
-              #:unless (and (= a 0) (= b 0)))
-    (cond [(in-bounds? (+ x a) (+ y b)) (roll? (+ x a) (+ y b))]
-          [else 0])))
+  (for*/sum ([a '(-1 0 1)] [b '(-1 0 1)] #:unless (and (= a 0) (= b 0)))
+            (cond
+              [(in-bounds? (+ x a) (+ y b)) (roll? (+ x a) (+ y b))]
+              [else 0])))
 
-(define remmed 0)
-(define removable (make-hash))
-(define (cycle removed removed-last)
-  (cond [(= removed-last 0) removed]
-        [else (begin
-                      (for* ([x width]
-                             [y height]
-                             #:when (= (roll? x y) 1))
-                        (if (< (neighbors x y) 4) (hash-set! removable (list x y) #t) (hash-set! removable (list x y) #f)))
-                      (for ([ht removable])
-                        (if (hash-ref removable ht) (begin (rem (car ht) (cadr ht)) (set! remmed (add1 remmed))) #f))
-                      (hash-clear removable)
-                      (cycle (+ removed remmed) remmed))]))
+(define (cycle)
+  (for*/sum ([x width] [y height] #:when (= (roll? x y) 1))
+            (if (< (neighbors x y) 4)
+                (begin
+                  (rem x y)
+                  1)
+                0)))
 
-(cycle 0 1)
+(define (iterate remmed)
+  (define new-rems (cycle))
+  (cond
+    [(= new-rems 0) remmed]
+    [else (iterate (+ remmed new-rems))]))
+
+(iterate 0)
